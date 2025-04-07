57f970843d9c15d24581e8570ba68aeaca6f8d17
BenXiaohai <1987884531@qq.com>
2025-04-07 16:43:34 +0800

    First Commit

---
+++ Project/Assignments/Assignment1/code/build/CMakeFiles/Rasterizer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rasterizer.dir/Triangle.obj"
+  "CMakeFiles/Rasterizer.dir/Triangle.obj.d"
+  "CMakeFiles/Rasterizer.dir/main.obj"
+  "CMakeFiles/Rasterizer.dir/main.obj.d"
+  "CMakeFiles/Rasterizer.dir/rasterizer.obj"
+  "CMakeFiles/Rasterizer.dir/rasterizer.obj.d"
+  "Rasterizer.exe"
+  "Rasterizer.exe.manifest"
+  "Rasterizer.pdb"
+  "libRasterizer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rasterizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
